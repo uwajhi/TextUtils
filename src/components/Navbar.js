123176118d1e646602c25e14d8b5f9e3cc91eb83
uwajhi <usamawajhi.pk@gmail.com>
2024-07-29 11:32:19 +0500

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Navbar = (props) => {
+  return (
+  <>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">{props.title}</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        {props.items.map((element, index) => (
+            <li className="nav-item" key={index}>
+              <a className="nav-link active" href="/">{element} </a>
+            </li>
+        ) 
+        )}
+        </ul>
+          <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+    </nav>
+  </>
+    
+  )
+}
+
+export default Navbar
