141e3545fc12aa4ae124a979a343e33a83e8ee31
uwajhi <usamawajhi.pk@gmail.com>
2024-08-02 17:47:04 +0500

    Alert added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
   return (
     <>
-      <Navbar title='TextUtils' items={['Home', 'About', 'Contact']}/>
-      <TextForm heading='Enter Text to Analyze'/>
+      <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert} />
+      <TextForm showAlert={showAlert} heading='Enter Text to Analyze' mode={mode} />
       {/* <About /> */}
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,15 +6,20 @@ const TextForm = (props) => {
 
     const handleUpClick = () => {
         setText(text.toUpperCase());
+        props.showAlert("Text has been coverted to Uppercase", "success");
     }
     const handleLowClick = () => {
         setText(text.toLowerCase());
+        props.showAlert("Text has been coverted to Lowercase", "success");
+
     }
     const handleOnChange = (event) => {
         setText(event.target.value);
     }
     const handleClearClick = () => {
         setText('');
+        props.showAlert("Text has been Cleared", "success");
+
     }
     const handleAlternatingClick = () => {
         var count = 0;
@@ -27,23 +32,30 @@ const TextForm = (props) => {
             count++;
         });
         setText(newText);
+        props.showAlert("Text has been coverted to Alternating Case", "success");
+
     }
     const handleCopyText = () => {
         navigator.clipboard.writeText(text);
+        props.showAlert("Text has been copied", "success");
+
     }
     const handleExtraSpaces = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(' '));
+        props.showAlert("Text has cleared of Extra Spaces", "success");
+
     }
     const handleFindOnChange = (event) => {
         setFindText(event.target.value);
+        props.showAlert("Text has been found", "success");
     }
   return (
     <>
-        <div className='container'>
+        <div className='container' style={{color: props.mode === 'light' ? '#042743' : 'white'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" id="exampleFormControlTextarea1" placeholder='Enter text here' rows="8" value={text} onChange={handleOnChange}></textarea>
+                <textarea className="form-control" style={{backgroundColor: props.mode === 'light' ? 'white' : 'grey', color: props.mode === 'light' ? '#042743' : 'white' }} id="exampleFormControlTextarea1" placeholder='Enter text here' rows="8" value={text} onChange={handleOnChange}></textarea>
             </div>
             <button className="btn btn-primary m-2" onClick={handleUpClick}>Convert to Upper Case</button>
             <button className="btn btn-primary m-2" onClick={handleLowClick}>Convert to Lower Case</button>
@@ -56,7 +68,7 @@ const TextForm = (props) => {
             <input type="text" name="find" id="find" value={findText} onChange={handleFindOnChange} />
             <p>Your string location is: {text.indexOf(findText)} </p>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode === 'light' ? '#042743' : 'white'}}>
             <h2>Your text summary</h2>
             <p>{text.length === 0 ? '0' : text.split(' ').length} words, {text.length} characters</p>
             <p>{text.length === 0 ? '0' : text.split(' ').length * 0.008} Minutes read</p>
